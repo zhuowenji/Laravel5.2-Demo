d9577cda5a7e63e8c24d6e08291f0a750ce8cb3c
zhuowenji <44208300@qq.com>
2016-06-28 02:08:06 +0800

    数据迁移，数据填充

---
+++ database/migrations/2016_06_27_172340_create_admins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 100);
+            $table->string('phone')->nullable();
+            $table->string('remember_token', 100)->nullable();
+            $table->string('password');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('admins');
+    }
+}
---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'username' => 'admin',
+            'password' => bcrypt('sdfsdf'),
+            'phone'    => '18310526441',
+        ]);
+    }
+}
